90fb42ffa30bc4f6d468d64d43e0ba1d47b81935
86LAK <lakshan392@gmail.com>
2024-04-28 14:50:44 +1000

    always false

--- backend/__tests__/backend.test.js
+++ backend/__tests__/backend.test.js
@@ -1,7 +1,13 @@
-const { alwaysTrue } = require('../routes/routes');
+const { alwaysTrue, alwaysFalse } = require('../routes/routes');
 
 describe('testing index file', () => {
   test('empty string should result in zero', () => {
     expect(alwaysTrue()).toBe(true);
+  });
+});
+
+describe('testing index file', () => {
+  test('empty string should result in zero', () => {
+    expect(alwaysFalse()).toBe(false);
   });
 });
