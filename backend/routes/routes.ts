1c73ceaaac0b62d260eb36918780814ac413418c
JTrenerry <jacksontrenerry01@gmail.com>
2024-04-28 17:51:29 +1000

    Adds basic posts

--- backend/routes/routes.ts
+++ backend/routes/routes.ts
@@ -10,13 +10,118 @@ export const router = Router();
 // Routes
 
 /*
+ * Post Requests below
+ * ===================
+ *
+ * Inputs and params are described within HANDSHAKE.md
+ *
+ */
+
+// Deletes a comment
+router.post('/comments/:commentId/delete', async (req: Request, res: Response) => {
+    const commentID = req.params.commentId;
+    editComment(commentID, '', '');
+    res.status(200).json('Comment Deleted!');
+}
+
+// Edits a comment
+router.post('/comments/:commentId/edit', async (req: Request, res: Response) => {
+    const commentID = req.params.commentId;
+    editComment(commentID, req.body.commentText, req.body.commentPNG);
+    res.status(200).json('Comment Edited!');
+});
+
+// Sets a comment as correct
+router.post('/comments/:commentId/correct', async (req: Request, res: Response) => {
+    const commentID = req.params.commentId;
+    await db.query(`
+    UPDATE comments
+    SET iscorrect = true
+    WHERE commentsid = $1
+    `, [commentID]);
+    res.status(200).json('Corrected!');
+});
+
+// Endorses a comment
+router.post('/comments/:commentId/endorse', async (req: Request, res: Response) => {
+    const commentID = req.params.commentId;
+    await db.query(`
+    UPDATE comments
+    SET isendorsed = true
+    WHERE commentsid = $1
+    `, [commentID]);
+    res.status(200).json('Endorsed!');
+});
+
+// Downvotes a comment
+router.post('/comments/:commentId/downvote', async (req: Request, res: Response) => {
+    const commentID = req.params.commentId;
+    await db.query(`
+    UPDATE comments
+    SET downvotes = downvotes + 1
+    WHERE commentsid = $1
+    `, [commentID]);
+    res.status(200).json('Downvoted!');
+});
+
+// Upvotes a comment
+router.post('/comments/:commentId/upvote', async (req: Request, res: Response) => {
+    const commentID = req.params.commentId;
+    await db.query(`
+    UPDATE comments
+    SET upvotes = upvotes + 1
+    WHERE commentsid = $1
+    `, [commentID]);
+    res.status(200).json('Upvoted!');
+});
+
+// Adds a new comment to the database
+router.post('/comments', async (req: Request, res: Response) => {
+    const { parentCommentID, commentText, commentPNG, isCorrect, isEndorsed, upvotes, downvotes } = req.body;
+    await db.query(`
+    INSERT INTO comments (parentCommentID, commentText, commentPNG, isCorrect, isEndorsed, upvotes, downvotes)
+    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    `, [parentCommentID, commentText, commentPNG, isCorrect, isEndorsed, upvotes, downvotes]);
+    res.status(201).json('Comment Added!');
+});
+
+// Adds a new question to the database
+router.post('/questions', async (req: Request, res: Response) => {
+    const { examID, questionText, questionType, questionPNG } = req.body;
+    await db.query(`
+    INSERT INTO questions (examID, questionText, questionType, questionPNG)
+    VALUES ($1, $2, $3, $4)
+    `, [examID, questionText, questionType, questionPNG]);
+    res.status(201).json('Question Added!');
+});
+
+// Adds a new exam to the database
+router.post('/exams', async (req: Request, res: Response) => {
+    const { examYear, examSemester, examType, courseCode } = req.body;
+    await db.query(`
+    INSERT INTO exams (examYear, examSemester, examType, courseCode)
+    VALUES ($1, $2, $3, $4)
+    `, [examYear, examSemester, examType, courseCode]);
+    res.status(201).json('Exam Added!');
+}
+
+// Adds a new Course to the database
+router.post('/courses', async (req: Request, res: Response) => {
+    const { courseCode, courseName, courseDescription } = req.body;
+    await db.query(`
+    INSERT INTO courses (courseCode, courseName, courseDescription)
+    VALUES ($1, $2, $3)
+    `, [courseCode, courseName, courseDescription]);
+    res.status(201).json('Course Added!');
+});
+
+/*
  * Get Requests below
  * ==================
  *
  * See outputs and params in HANDSHAKE.md
  *
  */ 
-
 
 // Gets comment by comment id
 router.get('/comments/:commentId', async (req: Request, res: Response) => {
@@ -141,6 +246,15 @@ interface CommentObject {
 }
 
 // Helper functions
+
+// function to edit / delete a comment
+async function editComment(commentID: number, commentText: string, commentPNG: string) {
+    await db.query(`
+    UPDATE comments
+    SET commenttext = $1, commentpng = $2
+    WHERE commentsid = $3
+    `, [commentText, commentPNG, commentID]);
+}
 
 // function to nest comments into their parent comments
 export function nest(jsonData: any[]) {
