49729c96c635d497a58a21b8fdca281221707c59
86LAK <lakshan392@gmail.com>
2024-04-28 15:16:30 +1000

    implemented very basic testing for single nest and nest

--- backend/__tests__/backend.test.js
+++ backend/__tests__/backend.test.js
@@ -1,13 +1,41 @@
-const { alwaysTrue, alwaysFalse } = require('../routes/routes');
+const { alwaysTrue, alwaysFalse, single_nest, nest } = require('../routes/routes');
 
-describe('testing index file', () => {
-  test('empty string should result in zero', () => {
-    expect(alwaysTrue()).toBe(true);
+describe('nest function', () => {
+  test('should correctly nest comments', () => {
+    const comments = [
+      { commentsid: 1, parentcommentid: null },
+      { commentsid: 2, parentcommentid: 1 },
+      { commentsid: 3, parentcommentid: 1 },
+      { commentsid: 4, parentcommentid: 2 },
+    ];
+    const nestedComments = nest(comments);
+    expect(nestedComments).toEqual([
+      {
+        commentsid: 1,
+        parentcommentid: null,
+        children: [
+          { commentsid: 2, parentcommentid: 1, children: [{ commentsid: 4, parentcommentid: 2 }] },
+          { commentsid: 3, parentcommentid: 1 },
+        ],
+      },
+    ]);
   });
 });
 
-describe('testing index file', () => {
-  test('empty string should result in zero', () => {
-    expect(alwaysFalse()).toBe(false);
+
+describe('single_nest function', () => {
+  test('should correctly filter comments', () => {
+    const comments = [
+      { commentsid: 1, parentcommentid: null },
+      { commentsid: 2, parentcommentid: 1 },
+      { commentsid: 3, parentcommentid: 1 },
+      { commentsid: 4, parentcommentid: 2 },
+    ];
+    const filteredComments = single_nest(comments, 1);
+    expect(filteredComments).toEqual([
+      { commentsid: 2, parentcommentid: 1, children: [{ commentsid: 4, parentcommentid: 2 }] },
+      { commentsid: 3, parentcommentid: 1 },
+      { commentsid: 4, parentcommentid: 2 },
+    ]);
   });
 });
--- backend/routes/routes.ts
+++ backend/routes/routes.ts
@@ -143,7 +143,7 @@ interface CommentObject {
 // Helper functions
 
 // function to nest comments into their parent comments
-function nest(jsonData: any[]) {
+export function nest(jsonData: any[]) {
     const dataDict: { [id: number]: CommentObject } = {};
     jsonData.forEach(item => dataDict[item.commentsid] = item);
 
@@ -162,7 +162,7 @@ function nest(jsonData: any[]) {
 }
 
 // function to return one comment with its children
-function single_nest(jsonData: any[], commentID: number) {
+export function single_nest(jsonData: any[], commentID: number) {
     const dataDict: { [id: number]: CommentObject } = {};
     jsonData.forEach(item => dataDict[item.commentsid] = item);
 
@@ -178,16 +178,4 @@ function single_nest(jsonData: any[], commentID: number) {
 
     const resultJsonData = jsonData.filter(item => item.commentsid !== commentID);
     return resultJsonData;
-}
-
-
-//TODO will be removed later...
-// make a generic function that always returns true
-export function alwaysTrue() {
-    return true;
-}
-
-//TODO will be removed later...
-export function alwaysFalse() {
-    return false;
 }
