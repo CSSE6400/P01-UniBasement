79678c6999ff4a1dc5939620c955936f631cb982
86.LAK <lakshan392@gmail.com>
2024-05-02 00:52:33 +1000

    set up and teardown function being called

--- integration_tests/test_exams.py
+++ integration_tests/test_exams.py
@@ -6,20 +6,32 @@ from .base import BaseCase
 
 
 class TestUser(BaseCase):
-    def test_evan(self):
-        """
-        Checks for a 200 response from the evan endpoint
-        """
-        response = requests.get(self.host() + '/evan', headers={'Accept': 'application/json'})
-        self.assertEqual(200, response.status_code)
+    def setUp(self):
+        # Generate test data
+        requests.get(self.host() + '/sketch', headers={'Accept': 'application/json'})
 
-    def test_health(self):
-        """
-        Checks for a 200 response from the health endpoint
-        """
-        response = requests.get(self.host() + '/health', headers={'Accept': 'application/json'})
-        self.assertEqual(200, response.status_code)
+    def tearDown(self):
+        # Delete test data
+        requests.get(self.host() + '/sketchies', headers={'Accept': 'application/json'})
+        
 
+
+    # def test_course_post(self):
+    #     """
+    #     Checks for a 201 response from the /courses endpoint
+    #     Checks for the correct response message
+    #     """
+    #     course_data = {
+    #         "courseCode": "CSSE6400",
+    #         "courseName": "Software Architecture",
+    #         "courseDescription": "Doing some software architecture stuff with Richard and Evan (my bestie)"
+    #     }
+
+    #     response = requests.post(self.host() + '/courses', json=course_data, headers={'Accept': 'application/json'})
+
+    #     self.assertEqual(201, response.status_code)
+
+    #     self.assertEqual('Course Added!', response.json())
 
 
 
