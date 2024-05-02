23afaf1ed7b00cb28a357de11cc56d485527a326
86.LAK <lakshan392@gmail.com>
2024-05-02 13:22:05 +1000

    wrote test case for course post request.

--- integration_tests/test_courses.py
+++ integration_tests/test_courses.py
@@ -28,11 +28,53 @@ class TestUser(BaseCase):
         self.assertEqual('Course Added!', response.json())
 
 
-    def test_course_get_all(self):
+    def test_course_post_null_coursecode(self):
         """
-        Checks for a 200 response from the /courses endpoint
+        Checks for a 400 response from the /courses endpoint
+        Checks for the correct response message
         """
-        #TODO
+        course_data = {
+            "courseCode": "",
+            "courseName": "Software Architecture",
+            "courseDescription": "Doing some software architecture stuff with Richard and Evan (my bestie)"
+        }
+
+        response = requests.post(self.host() + '/courses', json=course_data, headers={'Accept': 'application/json'})
+
+        self.assertEqual(400, response.status_code)
+
+        self.assertEqual('Course Code cannot be null', response.json())
+
+
+    def test_course_post_duplicate_coursecode(self):
+        """
+        Checks for a 400 response from the /courses endpoint
+        Checks for the correct response message
+        """
+        course_data = {
+            "courseCode": "DECO2500",
+            "courseName": "Human Computer Interaction",
+            "courseDescription": "Models of action, perception, cognition and interaction in human-machine systems. Methods of interaction analysis and interaction representation. Human-machine system evaluation. Practical implementation. Introduction to user and use-centred design principles. Broader topics may include: societal considerations, groupware, multimedia, media perspectives."
+        }
+
+        response = requests.post(self.host() + '/courses', json=course_data, headers={'Accept': 'application/json'})
+        # Should post the course fine the first time
+        self.assertEqual(201, response.status_code)
+        self.assertEqual('Course Added!', response.json())
+
+        # Should error as duplicate when posting same course again.
+        response = requests.post(self.host() + '/courses', json=course_data, headers={'Accept': 'application/json'})
+
+        self.assertEqual(400, response.status_code)
+
+        self.assertEqual('Course Code already exists', response.json())
+
+
+#TODO write test cases for above two cases on post
+
+
+
+
 
 
 if __name__ == '__main__':
