29608f514d54424f39dbaeeda83870d4351a10c5
86.LAK <lakshan392@gmail.com>
2024-05-03 12:25:33 +1000

    tests for post question completed

--- integration_tests/test_questions.py
+++ integration_tests/test_questions.py
@@ -59,7 +59,61 @@ class TestQuestions(BaseCase):
         self.assertEqual('No changes made', response.json())
 
 
-    #TODO finished the first route for questions. so the rest need to be done
+    def test_post_add_question_successful(self):
+        """
+        Check for a 201 response from the /questions endpoint
+        Check for the correct response message
+        """
+        body = {
+            "examId": 1,
+            "questionText": "Do you use Arch?",
+            "questionType": "Multiple Choice",
+            "questionPNG": None
+        }
+
+        response = requests.post(self.host() + '/questions', json=body)
+        self.assertEqual(201, response.status_code)
+        self.assertEqual('Question added', response.json())
+
+
+    def test_post_add_question_invalid_exam_id(self):
+        """
+        Check for a 404 response from the /questions endpoint
+        Check for the correct response message
+        """
+        body = {
+            "examId": 868686,
+            "questionText": "This question is going to an invalid examId",
+            "questionType": "Multiple Choice",
+            "questionPNG": None
+        }
+
+        response = requests.post(self.host() + '/questions', json=body)
+        self.assertEqual(404, response.status_code)
+        self.assertEqual('ExamId not found', response.json())
+
+
+    def test_post_add_question_no_exam_id(self):
+        """
+        Check for a 400 response from the /questions endpoint
+        Check for the correct response message
+        """
+        body = {
+            "examId": None,
+            "questionText": "This question is going to an invalid examId",
+            "questionType": "Multiple Choice",
+            "questionPNG": None
+        }
+
+        response = requests.post(self.host() + '/questions', json=body)
+        self.assertEqual(400, response.status_code)
+        self.assertEqual('ExamId not found', response.json())
+
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
