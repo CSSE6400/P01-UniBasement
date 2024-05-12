5f70bcc6d82a685fd0ba84e1596050c392f817ac
JTrenerry <jacksontrenerry01@gmail.com>
2024-05-12 23:37:37 +1000

    Practice makes perfect

---
+++ frontend/src/app/evan/page.tsx
@@ -0,0 +1,23 @@
+export default function EVAN() {
+    return (
+        <main>
+            <div className="px-0 ">
+                <br></br>
+                <br></br>
+                <h1 className="flex items-center justify-center text-5xl font-bold animate-spin">EVAN</h1>
+                <br></br>
+                <br></br>
+                <p className="flex items-center justify-center animate-spin">EVAN is a course about the life of Jesus Christ.</p>
+                <p className="flex items-center justify-center animate-spin">MEOW</p>
+                <br></br>
+                <br></br>                
+                <br></br>
+                <br></br>
+                <div className="flex items-center justify-center">
+                    <img className=" animate-spin" src="https://media.licdn.com/dms/image/C4D03AQFN0VclYWVh0w/profile-displayphoto-shrink_800_800/0/1517512247146?e=1721260800&v=beta&t=n6_XhCqRz9nGQIPHUYcrka7pkMOvx9mIZsIcWqXCBD4"/>
+                </div>
+
+            </div>
+        </main>
+    );
+}
