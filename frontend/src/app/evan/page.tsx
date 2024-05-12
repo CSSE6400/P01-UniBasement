38b5a58557df11a2a99ed8c249677c8efa2cdd19
JTrenerry <jacksontrenerry01@gmail.com>
2024-05-12 23:46:14 +1000

    Done

--- frontend/src/app/evan/page.tsx
+++ frontend/src/app/evan/page.tsx
@@ -1,6 +1,6 @@
 export default function EVAN() {
     return (
-        <main>
+        <main style={{ backgroundColor: 'pink' }}>
             <div className="px-0 ">
                 <br></br>
                 <br></br>
@@ -13,9 +13,52 @@ export default function EVAN() {
                 <br></br>                
                 <br></br>
                 <br></br>
-                <div className="flex items-center justify-center">
-                    <img className=" animate-spin" src="https://media.licdn.com/dms/image/C4D03AQFN0VclYWVh0w/profile-displayphoto-shrink_800_800/0/1517512247146?e=1721260800&v=beta&t=n6_XhCqRz9nGQIPHUYcrka7pkMOvx9mIZsIcWqXCBD4"/>
-                </div>
+                <p className="flex items-center justify-center text-7xl"> GET WELL SOON</p>
+                <br></br>
+                <br></br>                
+                <br></br>
+                <br></br>
+                <li  className=" animate-spin">
+                    <div>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            width="240"
+                            height="240"
+                        >
+                            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="red" />
+                        </svg>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img src="https://media.licdn.com/dms/image/C4D03AQFN0VclYWVh0w/profile-displayphoto-shrink_800_800/0/1517512247146?e=1721260800&v=beta&t=n6_XhCqRz9nGQIPHUYcrka7pkMOvx9mIZsIcWqXCBD4"/>
+                    </div>
+                    <br></br>
+                    <br></br>                
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>                
+                    <br></br>
+                    <br></br>
+                    <div>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            width="240"
+                            height="240"
+                        >
+                            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="red" />
+                        </svg>
+                    </div>
+                </li>
+                <br></br>
+                <br></br>                
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>                
+                <br></br>
+                <br></br>
 
             </div>
         </main>
