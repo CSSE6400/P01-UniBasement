682f412807f8d2bbea176f46421c9f6736f0e47f
liv <o.ronda@uqconnect.edu.au>
2024-05-12 11:31:30 +1000

    Updated card pointer events

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -5,8 +5,8 @@ function Card({ children }: PropsWithChildren) {
     <div
       className="group relative flex rounded-2xl bg-zinc-50 transition-shadow hover:shadow-md hover:shadow-zinc-900/5 dark:bg-white/2.5 dark:hover:shadow-black/5"
     >
-      <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-zinc-900/7.5 group-hover:ring-zinc-900/10 dark:ring-white/10 dark:group-hover:ring-white/20" />
-      <div className="rounded-2xl px-4 pb-4 pt-8">
+      <div className="pointer-events-none absolute inset-0 rounded-2xl ring-1 ring-inset ring-zinc-900/7.5 group-hover:ring-zinc-900/10 dark:ring-white/10 dark:group-hover:ring-white/20" />
+      <div className="rounded-2xl px-4 pb-4 pt-4 w-full">
           {children}
       </div>
     </div>
