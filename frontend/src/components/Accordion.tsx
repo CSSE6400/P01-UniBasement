46768ed82470c6bf5de3be55aab8eb42e0914645
liv <o.ronda@uqconnect.edu.au>
2024-05-12 12:58:14 +1000

    Initial exam page

---
+++ frontend/src/components/Accordion.tsx
@@ -0,0 +1,29 @@
+import { ReactNode, useState } from 'react';
+import { IconChevronDown, IconChevronUp } from '@tabler/icons-react';
+
+function Accordion({preview, content, forceOpen}: { preview: ReactNode, content: ReactNode, forceOpen?: boolean }) {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <div id="accordion-collapse">
+            <h2 id="accordion-collapse-heading">
+                <button
+                    type="button"
+                    className="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 dark:text-gray-400 gap-3"
+                    onClick={() => setOpen((prevOpen) => !prevOpen)}
+                >
+                    {preview}
+                    {(open || forceOpen) ? <IconChevronDown/> : <IconChevronUp/>}
+                </button>
+            </h2>
+            <div id="accordion-collapse-body" className={`${(open || forceOpen) ? '' : 'hidden'}`}>
+                <div className="py-5">
+                    {content}
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Accordion;
