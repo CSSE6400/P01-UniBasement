e8ec51c9efa3c259f12157884cb5cbbbc6856cbb
PramithKodali <pramithkodali14@gmail.com>
2024-05-13 23:01:49 +1000

    added functionality for rating - api doesn't work not sure why

--- frontend/src/app/courses/[courseCode]/course-profile/page.tsx
+++ frontend/src/app/courses/[courseCode]/course-profile/page.tsx
@@ -1,18 +1,23 @@
 'use client'
-import useCourse from '@/api/useCourse'
-import requireAuth from '@/app/requireAuth'
-import useExams from '@/api/useExams'
-import { Course } from '@/types'
-import Link from 'next/link'
-import { useState } from 'react'
+import useCourse from '@/api/useCourse';
+import useExams from '@/api/useExams';
+import { Course } from '@/types';
+import Link from 'next/link';
+import requireAuth from '@/app/requireAuth';
+import { SetStateAction, useEffect, useState } from 'react';
+import { IconStar, IconStarFilled, IconStarHalfFilled, IconStarHalf } from '@tabler/icons-react';
+import Card from '@/components/Card';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
-function CourseDescription({ course }: { course: Course }) {
-  return (
-    <div>
-      <h3 className="text-bold mt-2 text-2xl">Course Description</h3>
-      <p>{course!.courseDescription}</p>
-    </div>
-  )
+function CourseDescription({ course} : { course: Course }) {
+
+    return (
+        <div>
+            <h3 className='text-2xl text-bold mt-2'>Course Description</h3>
+            <p>{course!.courseDescription}</p>
+        </div>
+        
+    )
 }
 
 function CourseExams({ course} : { course: Course }) {
@@ -25,18 +30,18 @@ function CourseExams({ course} : { course: Course }) {
             {!isError && !isLoading && exams && (
                 <div className='flex flex-wrap space-x-4'>
                     {exams.map(exam => (
-                        <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <Card>
                             <a href="#">
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Semester {exam!.examSemester} - {exam!.examYear}</h5>
                             </a>
                             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"> {exam!.examType}</p>
-                            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-emerald-400 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800">
                                 View
                                 <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                                 </svg>
                             </a>
-                        </div>
+                        </Card>
                     ))}
                 </div>
             )}
@@ -45,29 +50,101 @@ function CourseExams({ course} : { course: Course }) {
     )
 }
 
-function CourseAssessment({ course} : { course: Course }) {
+
+function CourseRating ({ course } : { course: Course }) {
+    const [rating, setRating] = useState(Number(localStorage.getItem('rating')) || 0);
+    const [tempRating, setTempRating] = useState(rating);
+    const { user, error, isLoading } = useUser()
+    let endpoint = `${process.env.API_URL}/api/courses/`
+    let call = endpoint + course.courseCode + '/star'
+
+    const patchRating = async (starId: number) => {
+        if (!isLoading && user) {
+            try {
+                const response = await fetch(call,  {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        starRating: starId,
+                        userId: user.sub
+                    }),
+                });
+
+                if (!response.ok) {
+                    throw new Error('Error with PATCH requests')
+                }
+            } catch (error) {
+                console.error('Error:', error)
+            }
+        }
+    }
+
+    const handleMouseOver = (starId: number) => {
+        setTempRating(starId);
+    };
+
+    const handleMouseOut = () => {
+        setTempRating(rating)
+    };
+
+    const handleSaveRating = (starId: number) => {
+        setRating(starId);
+        localStorage.setItem('rating', starId.toString());
+        patchRating(starId)
+    };
+
+    useEffect(() => {
+        setTempRating(rating);
+    }, [rating]);
 
     return (
-        <div className='pl-0'>
-            <article className='text-pretty'>
-                <h4 className='text-2xl text-bold mt-2'>Cloud Infrastructure Assignment</h4>
-                <span className='flex'><p>Type: </p> <p>Computer Exercise</p></span>
-                <p>Task Description:</p>
-                <p className='text-balance'>
-                    You will implement a software system to meet specified functional and non-functional requirements. The implementation will be deployed on a
-                    cloud infrastructure and stress tested. You are required to design the system and select the appropriate compute platforms to deliver specified requirements.
-                    This assessment is to be delivered in three stages. Each stage is worth 10% of your final grade.
-                </p>
-            </article>
+        <div>
+            {error && <div> Shit has failed </div>}
+            {isLoading && <p>Loading...</p>}
+            {!user && (
+                <div>
+                    Please login to leave your rating
+                </div>
+            )}
+            {!error && !isLoading && user && (
+                        <Card>
+
+                        <div>
+                            <h2 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-3xl lg:text-3xl flex flex-row items-stretch">
+                                Leave your rating:
+                            </h2>
+                            <div className='flex flex-row pt-2 place-items-end'>
+                                {[...Array(5)].map((star, i) => {
+                                    const starId = i + 1;
+                                    return (
+                                    <span
+                                        key={starId}
+                                        className={`cursor-pointer text-2xl mx-1 ${starId <= tempRating ? 'text-yellow-400' : 'text-gray-300'}`}
+                                        onMouseOver={() => handleMouseOver(starId)}
+                                        onMouseOut={handleMouseOut}
+                                        onClick={() => handleSaveRating(starId)}
+                                    >
+                                        <IconStarFilled />
+                                    </span>
+                                    );
+                                })}
+                            </div>
+                        </div>
+
+                </Card>
+            )}
         </div>
     )
 }
 
 function CourseProfileTabs({ course } : { course: Course }) {
     const tabs = [
-        { name: "Assessment", content: <CourseAssessment course={course} /> },
+        { name: "Assessment", content: <CourseDescription course={course} /> },
         { name: "Past Exams", content: <CourseExams course={course} />},
-        { name: "Calendar", content: "Not ready yet..." }
+        { name: "Calendar", content: "Not ready yet..." },
+        { name: "Rate The Course", content: <CourseRating course={course} />}
       ];
       const [currentTab, setCurrentTab] = useState(tabs[0].name);
 
@@ -135,13 +212,29 @@ function CourseHeader({ course }: { course: Course }) {
       <CourseDescription course={course}></CourseDescription>
     </div>
   )
+
+    return (
+        <div>
+            <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl flex flex-row">
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-emerald-400 to-emerald-200 pb-4">
+                    {course!.courseCode}: {course!.courseName}
+                </span>
+            </h1>
+            <h2 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-3xl lg:text-3xl flex flex-row items-center">
+                <IconStarFilled /> {val} Course Rating - {votes} Ratings
+            </h2>
+
+
+        </div>
+
+    );
 }
 
 function CourseProfile({ params }: { params: { courseCode: string } }) {
   const { course, isError, isLoading } = useCourse(params.courseCode)
 
   return (
-    <main>
+    <main className='mx-auto  w-full max-w-4xl'>
       {isError && <div> Shit has failed </div>}
       {isLoading && <p>Loading...</p>}
       {!isError && !isLoading && course && (
