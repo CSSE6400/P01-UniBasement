b13eb6c41474fee72e937a2f20b3f295377488d5
liv <o.ronda@uqconnect.edu.au>
2024-05-14 17:06:10 +1000

    Some styling updates

--- frontend/src/app/courses/page.tsx
+++ frontend/src/app/courses/page.tsx
@@ -7,6 +7,7 @@ import { backendCourseToFrontend } from '@/lib/courseUtils'
 import requireAuth from '../requireAuth'
 import { usePinned } from '@/api/usePins';
 import { useMemo } from 'react';
+import Title from '@/components/Title';
 
 function Courses() {
   const { courses, isError, isLoading } = useCourses()
@@ -30,23 +31,26 @@ function Courses() {
 
   return (
     <main>
-      {isLoading && <p>Loading...</p>}
-      <div className="m-5 grid grid-cols-4 gap-4">
-        {!isError &&
-          !isLoading &&
-          displayCourses?.map((course) => (
-            <CourseCard
-              course={course}
-              key={course.code}
-            />
-          ))}
-        <Card>
-          {/* TODO: Make this do something */}
-          <h3 className="text-sm font-semibold leading-7 text-zinc-900 dark:text-white">
-            Add Course <IconCirclePlus />
-          </h3>
-        </Card>
-      </div>
+        <div className="max-w-4xl w-full mx-auto flex flex-col items-center justify-center">
+            <Title title="Courses" />
+          {isLoading && <p>Loading...</p>}
+          <div className="m-5 grid grid-cols-4 gap-4">
+            {!isError &&
+              !isLoading &&
+              displayCourses?.map((course) => (
+                <CourseCard
+                  course={course}
+                  key={course.code}
+                />
+              ))}
+            <Card>
+              {/* TODO: Make this do something */}
+              <h3 className="text-sm font-semibold leading-7 text-zinc-900 dark:text-white">
+                Add Course <IconCirclePlus />
+              </h3>
+            </Card>
+          </div>
+        </div>
     </main>
   )
 }
