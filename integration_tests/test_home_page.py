5cf178b49d2e68a36475c9b34fc802405bbe9b48
86.LAK <lakshan392@gmail.com>
2024-05-14 21:34:59 +1000

    crazy in depth front end testing

--- integration_tests/test_home_page.py
+++ integration_tests/test_home_page.py
@@ -1,21 +1,25 @@
-# import re
-# from playwright.sync_api import Page, expect
-# import unittest
-# import requests
-# from .base import BaseCase
+import re
+import pytest
+from playwright.sync_api import sync_playwright
 
-# class TestFrontEnd(BaseCase):
-#     def test_has_title(page: Page):
-#         page.goto("https://playwright.dev/")
-    
-#         # Expect a title "to contain" a substring.
-#         expect(page).to_have_title(re.compile("Playwright"))
-    
-#     def test_get_started_link(page: Page):
-#         page.goto("https://playwright.dev/")
-    
-#         # Click the get started link.
-#         page.get_by_role("link", name="Get started").click()
-    
-#         # Expects page to have a heading with the name of Installation.
-#         expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+
+class TestFrontEnd:
+    @pytest.fixture(scope="class")
+    def page(self):
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            page = browser.new_page()
+            yield page
+            page.close()
+            browser.close()
+
+    def test_has_title(self, page):
+        page.goto("http://localhost:3000")
+        assert re.compile("EVAN").search(
+            page.title()), "Title does not contain 'Evan'"
+
+    # def test_get_started_link(self, page):
+    #     page.goto("https://localhost:3000")
+    #     page.click("text=Get started")
+    #     assert page.wait_for_selector(
+    #         "text=Installation"), "Installation heading not found"
