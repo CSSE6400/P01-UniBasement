ef09bc007173e7caf3be00443283bf53e11eecc8
liv <o.ronda@uqconnect.edu.au>
2024-05-17 20:03:21 +1000

    Save user to database on account creation

--- frontend/src/app/api/auth/[auth0]/route.ts
+++ frontend/src/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,22 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleCallback, Session } from '@auth0/nextjs-auth0';
+import { NextApiRequest } from 'next';
 
-export const GET = handleAuth();
+const afterCallback = async (
+    req: NextApiRequest,
+    session: Session,
+) => {
+    // add the user to our database
+    await fetch(`${process.env.API_URL}/api/users`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userId: session.user.sub }),
+    });
+    return session;
+};
+
+export const GET = handleAuth({
+    // @ts-ignore
+    callback: handleCallback({ afterCallback })
+});
